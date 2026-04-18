3a8624bf9b70fa9c1fcbfa6b9faeff12c6491756
ekudravceva <ekudravceva052@gmail.com>
2026-04-18 23:03:43 +0300

    Initial Commit

---
+++ App/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  App
+//
+//  Created by Екатерина on 18.04.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
